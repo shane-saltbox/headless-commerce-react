2e28ae237950009f6f310596ec63f9f8125e10c8
shane-saltbox <89033396+shane-saltbox@users.noreply.github.com>
2023-01-27 13:25:41 -0600

    update

--- routes/productDetail.js
+++ routes/productDetail.js
@@ -2,8 +2,10 @@ const db = require('../db');
 const randomstring = require('randomstring');
 const uuid = require('uuid');
 var jsforce = require('jsforce');
+const axios = require('axios');
 
 const CONSTANTS = require('../constants');
+const { config } = require('dotenv');
 
 module.exports = function(app, debugLogger, config) {
 
@@ -15,63 +17,63 @@ module.exports = function(app, debugLogger, config) {
     const response = {...CONSTANTS.RESPONSE_OBJECT};
 
     //try {
-      const { id } = req.query;
+        const { id } = req.query;
 
-      if (!id) {
-        const error = new Error();
-        error.message = 'Required fields not found.';
-        error.status = 206;
+        if (!id) {
+            const error = new Error();
+            error.message = 'Required fields not found.';
+            error.status = 206;
 
-        throw(error);
-      }
-
-      var conn = new jsforce.Connection({
-        oauth2 : {
-          // you can change loginUrl to connect to sandbox or prerelease env.
-          // loginUrl : 'https://test.salesforce.com',
-          clientId : config.SF_CLIENT_ID,
-          clientSecret : config.SF_CLIENT_SECRET,
-          redirectUri : 'https://headless-commerce.herokuapp.com/callback'
+            throw(error);
         }
-      });
-      console.log('##DEBUG: '+conn);
-      conn.login(config.SF_USERNAME, config.SF_PASSWORD, function(err, userInfo) {
-        if (err) { return console.error(err); }
-        // Now you can get the access token and instance URL information.
-        // Save them to establish connection next time.
-        console.log('##DEBUG: '+conn.accessToken);
-        console.log('##DEBUG: '+conn.instanceUrl);
-        // logged in user property
-        console.log("User ID: " + userInfo.id);
-        console.log("Org ID: " + userInfo.organizationId);
-        // ...
-      });
 
-      var body = { 
-            effectiveAccountId: 'hello', 
-            skus: '100'
-        };
-        conn.apex.get("/commerce/webstores/webstoreId/products", body, function(err, res) {
-        if (err) { return console.error(err); }
-        console.log("response: ", res);
-        // the response object structure depends on the definition of apex class
+        var conn = new jsforce.Connection({
+            oauth2 : {
+            // you can change loginUrl to connect to sandbox or prerelease env.
+            // loginUrl : 'https://test.salesforce.com',
+            clientId : config.SF_CLIENT_ID,
+            clientSecret : config.SF_CLIENT_SECRET,
+            redirectUri : 'https://headless-commerce.herokuapp.com/callback'
+            }
+        });
+        console.log('##DEBUG: '+JSON.stringify(conn));
+        console.log('##DEBUG SF_USERNAME: '+JSON.stringify(config.SF_USERNAME));
+        console.log('##DEBUG SF_PASSWORD: '+JSON.stringify(config.SF_PASSWORD));
+        conn.login(config.SF_USERNAME, config.SF_PASSWORD, function(err, userInfo) {
+            if (err) { return console.error(err); }
+            // Now you can get the access token and instance URL information.
+            // Save them to establish connection next time.
+            console.log('##DEBUG: '+JSON.stringify(conn.accessToken));
+            console.log('##DEBUG: '+JSON.stringify(conn.instanceUrl));
+            // logged in user property
+            console.log("User ID: " + userInfo.id);
+            console.log("Org ID: " + userInfo.organizationId);
+            // ...
         });
 
-      if (DEBUG === 'true') debugLogger.info('/api/checkInventory', 'GET', id, 'Inventory Level query.', query_inventory);
+        const params = {
+            effectiveAccountId: '0015e00000MMkzQAAT',
+            skus: '800984'
+          };
+          
+        const res = await axios.get(config.SF_LOGIN_URL+'/commerce/webstores/webstoreId/products', { params });
 
 
-      if (!response_inventory || !response_inventory.rows.length) {
-        const error = new Error();
-        error.message = 'Inventory not found.';
-        error.status = 404;
+        if (DEBUG === 'true') debugLogger.info('/api/checkInventory', 'GET', id, 'Inventory Level query.', query_inventory);
 
-        throw(error);
-      }
 
-      response.data = response_inventory.rows;
-      response.success = true;
+        if (!response_inventory || !response_inventory.rows.length) {
+            const error = new Error();
+            error.message = 'Inventory not found.';
+            error.status = 404;
 
-      res.status(200).send(response);
+            throw(error);
+        }
+
+        response.data = response_inventory.rows;
+        response.success = true;
+
+        res.status(200).send(response);
 
     /* } catch (error) {
       const { id } = req.query;
