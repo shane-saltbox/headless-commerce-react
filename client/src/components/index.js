c497e68724190b43837b6b6236e5a1f8fbcd0d25
shane-saltbox <89033396+shane-saltbox@users.noreply.github.com>
2023-01-29 09:22:45 -0600

    update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as MySubscriptions } from './MySubscriptions';
 export { default as MyProduct } from './MyProductDetail';
+export { default as ProductFields } from './ProductFields';
