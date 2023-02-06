0b7e28bc6a4df9b3b7bb20aac04afbcaa9df696c
shane-saltbox <89033396+shane-saltbox@users.noreply.github.com>
2023-02-06 13:30:05 -0600

    .

--- client/src/landmarks/Header.jsx
+++ client/src/landmarks/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { isEqual } from 'lodash';
 import PropTypes from 'prop-types';
 import Skeleton from 'react-loading-skeleton';
-import { MenuCart } from "../components";
+import { MenuCart, NavMenu } from "../components";
 
 import AppContext from '../AppContext';
 
