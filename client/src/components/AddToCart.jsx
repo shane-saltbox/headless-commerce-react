327b03e8fc479781c0caaaac860adc3b5ff7b788
shane-saltbox <89033396+shane-saltbox@users.noreply.github.com>
2023-02-06 22:01:12 -0600

    .

--- client/src/components/AddToCart.jsx
+++ client/src/components/AddToCart.jsx
@@ -76,7 +76,7 @@ class AddToCart extends React.Component {
             /> */}
             {cartItems && cartItems.length > 0 ? (
                 <>
-                    {cartItems.find(e => e.cartItem.productDetails.sku === productSku).map((item) => {
+                    {cartItems.filter(e => e.cartItem.productDetails.sku === productSku).map((item) => {
                         return (
                             <div className='row'>
                                 <div className='col-lg-3'>
